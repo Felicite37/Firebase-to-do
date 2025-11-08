81d0bcd6003a65a54e82bdf8d89b798e928b738f
Felicite37 <feliciteniyo24@gmail.com>
2025-11-09 00:24:45 +0200

    test firebase connection

---
+++ src/firebase.ts
@@ -0,0 +1,17 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyCeMHxmjdsXklVhjiuic2wvs_MBYigNGbQ",
+  authDomain: "fir-todo-f5f38.firebaseapp.com",
+  projectId: "fir-todo-f5f38",
+  storageBucket: "fir-todo-f5f38.firebasestorage.app",
+  messagingSenderId: "980381841097",
+  appId: "1:980381841097:web:517029adc50ab82284bf18",
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
